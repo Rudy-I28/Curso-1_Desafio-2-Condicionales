9700c2fee2457a21d321a190e7c034617cdd5cd1
Rudy-I28 <rivan.jamjachi@gmail.com>
2024-06-21 23:58:35 -0500

    Curso 1 - Desafio 2: Alura Latam
    Condicionales y Concatenación

---
+++ app.js
@@ -0,0 +1,41 @@
+/*
+//Ejercio 1
+var diaSemana = prompt("Qué día de la semana es: ");
+
+if(diaSemana == "Sabado" || diaSemana == "Domingo"){
+    alert("¡Buen fin de semana!");
+} else{
+    alert("¡Buena semana!")
+}
+*/
+/*
+//Ejercicio 2
+let numero = prompt("Ingrese un número para determinar si es positivo o negativo: ");
+
+if(numero > 0){
+    alert("¡El número ingresado es positivo!");
+} else{
+    alert("¡El número ingresado es negativo!");
+}
+*/
+/*
+//Ejercicio 3
+let numero = prompt("Ingrese un número para ganar: ");
+
+if(numero >= 100){
+    alert("¡Felicidades, has ganado!");
+} else{
+    alert("¡Intentalo nuevamente para ganar!");
+}
+*/
+/*
+//Ejercicio 4
+let saldoCuenta = 900;
+
+alert(`El saldo de su cuenta es: ${saldoCuenta}`); //Template string(` `);
+*/
+
+//Ejercicio 5
+let nombreUsuario = prompt("Ingrese su nombre por favor: ");
+
+alert(`Bienvenid(o) ${nombreUsuario}`);
